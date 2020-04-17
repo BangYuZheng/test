f7d0aa14f6496c73001ce1e57a8bbb8439674a26
zhkm <james.jia@bjzhkm.com>
2020-04-17 22:07:30 +0800

    Initial commit

---
+++ src/com/test/Test.java
@@ -0,0 +1,42 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Test {
+    String a="";
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public static void main(String[] args) {
+        dsa();
+        System.out.println("test");
+        dsa();
+    }
+    //注释123333333
+
+    public static void dsa() {
+        System.out.println(1111);
+        System.out.println();
+        Date date = new Date();
+        List list=new ArrayList();
+        list.add(0,12);
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        if (list == null) {
+
+        }
+        for (int i = 0; i < list.size(); i++) {
+            
+        }
+    }
+
+}
